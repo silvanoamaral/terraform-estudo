7aaa16edd82cb89402fc2067f6e7a850cb9de085
silvanoamaral <silvano.amaral9@gmail.com>
2022-10-26 22:15:57 -0300

    criando bucket na aws˜

---
+++ criando-bucket-na-aws/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "silvano"
+      managed-by = "terraform"
+    }
+  }
+}
